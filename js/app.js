a0550b00fc600c702682551a3c0eb13d01701ae5
al45587 <al45587@un.edu.mx>
2025-07-15 09:43:00 -0600

    clase 15/07/25

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
-//Menu responsive
+// MENU RESPONSIVE
 var btnMenuOpen = document.getElementById('btnMenuOpen');
-var brnMenuClose = document.getElementById('btnMenuClose');
+var btnMenuClose = document.getElementById('btnMenuClose');
 var menubar = document.getElementById('menubar');
 
-btnMenuOpen.addEventListener('click',function(){
+btnMenuOpen.addEventListener('click', function() {
     menubar.classList.add('active');
 });
 
-btnMenuClose.addEventListener('click',function(){
+btnMenuClose.addEventListener('click', function() {
     menubar.classList.remove('active');
-
 });
 
 var enlaces = document.getElementById('enlaces');
-enlaces.addEventListener('click',function(){
+enlaces.addEventListener('click', function() {
     menubar.style.transitionDelay = '0.5s';
     menubar.classList.remove('active');
 });
+
+
